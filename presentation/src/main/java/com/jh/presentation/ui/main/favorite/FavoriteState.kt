f89a92f7b111a1d8d5af993cc88e5cef73427e85
jangjh123 <gn0001231@gmail.com>
2023-03-25 00:09:11 +0900

    [ui] FavoriteActivity UI 전체 구현

---
+++ presentation/src/main/java/com/jh/presentation/ui/main/favorite/FavoriteState.kt
@@ -0,0 +1,9 @@
+package com.jh.presentation.ui.main.favorite
+
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.ModalBottomSheetValue
+
+@OptIn(ExperimentalMaterialApi::class)
+data class FavoriteState(
+    val bottomSheetStateValue: ModalBottomSheetValue = ModalBottomSheetValue.Hidden
+)
